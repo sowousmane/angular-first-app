24545f023b9667ad0b7e76158b6f010eed394db0
sowousmane <sowousmane4811@gmail.com>
2021-09-07 16:08:09 +0200

    feat: todo list plus add and remove todo items

---
+++ src/app/components/todos/todos.component.ts
@@ -0,0 +1,44 @@
+import { Todo } from './../../modules/Todo';
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-todos',
+  templateUrl: './todos.component.html',
+  styleUrls: ['./todos.component.css'],
+})
+export class TodosComponent implements OnInit {
+  todos: Todo[] | undefined;
+  inputTodo: string = '';
+
+  constructor() {}
+
+  ngOnInit(): void {
+    this.todos = [
+      {
+        content: 'first todo',
+        completed: false,
+      },
+      {
+        content: 'second todo',
+        completed: false,
+      },
+    ];
+  }
+  toggleDone(id: number) {
+    this.todos?.map((v, i) => {
+      if (i == id) v.completed = !v.completed;
+      return v;
+    });
+  }
+
+  deleteTodo(id: number) {
+    this.todos = this.todos?.filter((v, i) => i != id);
+  }
+  addTodo() {
+    this.todos?.push({
+      content: this.inputTodo,
+      completed: false,
+    });
+    this.inputTodo = '';
+  }
+}
